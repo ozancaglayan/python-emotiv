09a76682985e232cb4ef326d5de909d3f9ea4542
Ozan Çağlayan <ozancag@gmail.com>
2013-05-15 15:19:09 +0300

    utils: Add script to simulate dongle replug

---
+++ utils/epoc-replug.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import glob
+import sys
+
+if __name__ == "__main__":
+    epoc = None
+
+    for dev in glob.glob("/sys/bus/usb/devices/*"):
+        try:
+            manu = open(os.path.join(dev, "manufacturer")).read().strip()
+            if manu == "Emotiv Systems Pty Ltd":
+                epoc = os.path.basename(dev)
+        except:
+            pass
+
+    with open("/sys/bus/usb/drivers/usb/unbind", "w") as unbind:
+        unbind.write(os.path.basename(epoc))
+    with open("/sys/bus/usb/drivers/usb/bind", "w") as bind:
+        bind.write(os.path.basename(epoc))
+
+
