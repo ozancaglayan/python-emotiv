5414b219f2720b03632d35441378ee4989ad540b
Ozan Çağlayan <ozancag@gmail.com>
2013-05-15 15:18:23 +0300

    add utils.py

---
+++ emotiv/utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# vim:set et ts=4 sw=4:
+#
+## Copyright (C) 2012 Ozan Çağlayan <ocaglayan@gsu.edu.tr>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+def check_packet_drops(seq_numbers):
+
+    cnt = 0
+    for seq in xrange(len(seq_numbers) - 1):
+        cur = int(seq_numbers[seq])
+        _next = int(seq_numbers[seq + 1])
+        cnt += (((cur + 1) % 128) - _next)
+    return cnt
