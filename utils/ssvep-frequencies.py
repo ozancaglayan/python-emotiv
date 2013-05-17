218bca6a6a5ec1479271580c9a8f0c7191624cc9
Ozan Çağlayan <ozancag@gmail.com>
2013-05-17 17:41:22 +0300

    utils: Improve plotting utility

--- utils/ssvep-frequencies.py
+++ utils/ssvep-frequencies.py
@@ -5,13 +5,15 @@ import sys
 
 import numpy as np
 
-from scipy import fftpack
+from scipy import fftpack, signal
 from scipy.io import loadmat
 from matplotlib import pylab as plt
 
+from emotiv import utils
+
 if __name__ == '__main__':
     if len(sys.argv) > 2:
-        ch = sys.argv[2]
+        ch = list(sys.argv[2:])
     else:
         ch = None
 
@@ -29,21 +31,27 @@ if __name__ == '__main__':
         pidxs = np.where(sample_freq > 0)
         freqs = sample_freq[pidxs]
 
+        resting_drops = utils.check_packet_drops(resting_sig['SEQ'][0,:])
+        ssvep_drops = utils.check_packet_drops(ssvep_sig['SEQ'][0,:])
+        if ssvep_drops:
+            print "SSVEP drops: %s" % ssvep_drops
+        if resting_drops:
+            print "Resting drops: %s" % resting_drops
+
         if ch:
-            fig, axarr = plt.subplots(4, 1, sharex=False)
-            axarr = np.array(axarr).reshape((-1, 1))
-            channels = [ch]
+            fig, axarr = plt.subplots(5, len(ch), sharex=False)
+            axarr = np.array(axarr).reshape((-1, len(ch)))
+            channels = ch
         else:
             # 3 for matlab meta data arrays, and 1 for SEQ
-            fig, axarr = plt.subplots(4, len(ssvep_sig) - 4, sharex="col",
-                    sharey="row")
-            print axarr
-            channels = [c for c in ssvep_sig.keys() if not c.startswith(("__",
-                "SEQ"))]
+            fig, axarr = plt.subplots(5, len(ssvep_sig) - 4, sharex=False)
+            channels = [c for c in ssvep_sig.keys() if not c.startswith(("__", "SEQ"))]
 
         for index, channel in enumerate(channels):
-            rest = resting_sig[channel][0,:]
-            ssvep = ssvep_sig[channel][0,:]
+            rest = signal.detrend(resting_sig[channel][0,:], type='constant')
+            ssvep = signal.detrend(ssvep_sig[channel][0,:], type='constant')
+            #rest = resting_sig[channel][0,:]
+            #ssvep = ssvep_sig[channel][0,:]
             diff_signal = ssvep - rest
 
             rest_fft = fftpack.fft(rest)
@@ -59,14 +67,23 @@ if __name__ == '__main__':
             rest_power = (rest_power / rest_power.max()) * 100
             ssvep_power = (ssvep_power / ssvep_power.max()) * 100
             sig_diff_power = (sig_diff_power / sig_diff_power.max()) * 100
+            """
+            freqs, rest_power = signal.welch(rest, fs=128, scaling='spectrum')
+            freqs, ssvep_power = signal.welch(ssvep, fs=128)
+            freqs, sig_diff_power = signal.welch(diff_signal, fs=128)
+            """
 
-            axarr[0, index].plot(freqs, rest_power)
-            axarr[0, index].set_title("Resting(%s)" % channel)
-            axarr[1, index].plot(freqs, ssvep_power)
-            axarr[1, index].set_title("SSVEP(%s)" % channel)
-            axarr[2, index].plot(freqs, ssvep_power - rest_power)
-            axarr[2, index].set_title("Difference(%s)" % channel)
-            axarr[3, index].plot(freqs, sig_diff_power)
-            axarr[3, index].set_title("Signal Difference(%s)" % channel)
+            axarr[0, index].plot(ssvep)
+            axarr[0, index].plot(rest, color='r')
+            axarr[0, index].set_title("EEG(%s)" % channel)
+            axarr[1, index].plot(freqs, rest_power)
+            axarr[1, index].set_title("Resting(%s)" % channel)
+            axarr[2, index].plot(freqs, ssvep_power)
+            axarr[2, index].set_title("SSVEP(%s)" % channel)
+            axarr[3, index].plot(freqs, ssvep_power - rest_power)
+            axarr[3, index].set_title("Difference(%s)" % channel)
+            axarr[4, index].plot(freqs, sig_diff_power)
+            axarr[4, index].set_title("Signal Difference(%s)" % channel)
 
+        fig.tight_layout()
         plt.show()
