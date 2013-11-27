59ed339d18ecf76a6e48860d7285647032cba38b
Ozan Çağlayan <ozancag@gmail.com>
2013-11-27 18:00:54 +0200

    lsl: Improve

--- lsl/lsl-pyemotiv.py
+++ lsl/lsl-pyemotiv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pylsl
+import time
 
 try:
     from emotiv import epoc
@@ -11,9 +12,9 @@ except ImportError:
 if __name__ == '__main__':
 
     headset = epoc.EPOC()
-    print "Found headset: ", headset.serial_number
+    print "Found headset with serial number: ", headset.serial_number
 
-    info = pylsl.stream_info('EPOC', 'EEG', len(headset.channel_mask),
+    info = pylsl.stream_info('Emotiv EEG', 'EEG', len(headset.channel_mask),
                              headset.sampling_rate,
                              pylsl.cf_int16,
                              str(headset.serial_number))
@@ -26,11 +27,16 @@ if __name__ == '__main__':
         channels.append_child("channel").append_child_value("label", ch).append_child_value("unit","microvolts").append_child_value("type","EEG")
 
     # Outgoing buffer size = 360 seconds, transmission chunk size = 32 samples
-    outlet = pylsl.stream_outlet(info, 360, 32)
+    outlet = pylsl.stream_outlet(info, 1, 32)
 
     while True:
-        s = headset.get_sample()
-        if s:
-            outlet.push_sample(pylsl.vectori(s), pylsl.local_clock())
+        try:
+            s = headset.get_sample()
+        except epoc.EPOCTurnedOffError, e:
+            print "Headset is turned off, waiting..."
+            time.sleep(0.02)
+        else:
+            if s:
+                outlet.push_sample(pylsl.vectori(s), pylsl.local_clock())
 
     headset.disconnect()
