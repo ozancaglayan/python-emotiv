4f39c3ca9eaa99a4a91d4d01060bf8fe50c5db62
Ozan Çağlayan <ozancag@gmail.com>
2013-12-10 12:11:45 +0200

    utils: Generate filename dynamically if it is None

--- emotiv/utils.py
+++ emotiv/utils.py
@@ -20,6 +20,8 @@
 from scipy.io import savemat
 import numpy as np
 
+import time
+
 def check_packet_drops(seq_numbers):
     lost = []
     for seq in xrange(len(seq_numbers) - 1):
@@ -38,12 +40,9 @@ def get_level(raw_data, bits):
         level |= (ord(raw_data[b]) >> o) & 1
     return level
 
-def save_as_matlab(_buffer, channel_mask, filename, metadata=None):
+def save_as_matlab(_buffer, channel_mask, filename=None, metadata=None):
     """Save as matlab data with optional metadata."""
     matlab_data = {"CTR": _buffer[:, 0]}
-
-    if not filename.endswith(".mat"):
-        filename = "%s.mat" % filename
 
     nr_samples = _buffer[:, 0].size
     trial = np.zeros((1,), dtype=np.object)
@@ -67,5 +66,15 @@ def save_as_matlab(_buffer, channel_mask, filename, metadata=None):
     if metadata:
         for key, value in metadata.items():
             matlab_data[key] = value
+
+    # Put time of recording
+    date_info = time.strftime("%d-%m-%Y_%H-%M-%S")
+    matlab_data["date"] = date_info
+
+    if not filename:
+        if metadata and metadata.has_key("Initials"):
+            filename = "emotiv-%s-%s.mat" % (metadata["Initials"], date_info)
+        else:
+            filename = "emotiv-%s.mat" % date_info
 
     savemat(filename, matlab_data, oned_as='row')
